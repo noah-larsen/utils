0499c0c71bd34ebd21db461e54ef8ee11ab006ac
Noah Larsen <noah.larsen@bbva.com>
2018-07-18 14:49:05 -0500

    added support for reading the google spreadsheet modified registration date from the work data dictionary

--- src/main/scala/dataDictionary/FieldEntryReaderWriter.scala
+++ src/main/scala/dataDictionary/FieldEntryReaderWriter.scala
@@ -43,7 +43,7 @@ case class FieldEntryReaderWriter(ingestionStage: IngestionStage) extends DataRe
       Some(Try(Some(row(19).toInt)).getOrElse(None)).filter(_.isDefined),
       dropDown(FieldGeneratedValues, row(20)),
       text(row(21)),
-      Some(row(22)).filter(_ != new String).map(LocalDate.parse(_, DateTimeFormatter.ofPattern(FieldEntryReaderWriter.registrationDateFormat))),
+      Some(row(22)).filter(_ != new String).map(x => Seq(FieldEntryReaderWriter.registrationDateFormat, FieldEntryReaderWriter.googleSpreadsheetModifiedDateFormat).map(y => Try(LocalDate.parse(x, DateTimeFormatter.ofPattern(y)))).maxBy(_.isSuccess).get),
       dropDown(Countries, row(23)),
       text(row(24)),
       text(row(25)),
@@ -63,55 +63,57 @@ case class FieldEntryReaderWriter(ingestionStage: IngestionStage) extends DataRe
 
 object FieldEntryReaderWriter {
 
-    object FieldEntryColumns extends AbstractColumns[FieldEntry] {
+  object FieldEntryColumns extends AbstractColumns[FieldEntry] {
 
-      override type Column = FieldEntryColumn
+    override type Column = FieldEntryColumn
 
-      sealed class FieldEntryColumn(val string: FieldEntry => String) extends AbstractColumn[FieldEntry]
-
-
-      case object Country extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.country))
-      case object PhysicalNameObject extends FieldEntryColumn(x => stringOptionToString(x.physicalNameObject))
-      case object StorageType extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.storageType))
-      case object StorageZone extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.storageZone))
-      case object PhysicalNameField extends FieldEntryColumn(x => stringOptionToString(x.physicalNameField))
-      case object LogicalNameField extends FieldEntryColumn(x => stringOptionToString(x.logicalNameField))
-      case object SimpleFieldDescription extends FieldEntryColumn(x => stringOptionToString(x.simpleFieldDescription))
-      case object Catalog extends FieldEntryColumn(x => stringOptionToString(x.catalog))
-      case object DataType extends FieldEntryColumn(x => stringOptionToString(x.dataType))
-      case object Format extends FieldEntryColumn(x => stringOptionToString(x.format))
-      case object LogicalFormat extends FieldEntryColumn(x => stringOptionToString(x.logicalFormat))
-      case object Key extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.key))
-      case object Mandatory extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.mandatory))
-      case object DefaultValue extends FieldEntryColumn(x => stringOptionToString(x.defaultValue))
-      case object PhysicalNameSourceObject extends FieldEntryColumn(x => stringOptionToString(x.physicalNameSourceObject))
-      case object SourceField extends FieldEntryColumn(x => stringOptionToString(x.sourceField))
-      case object DataTypeSourceField extends FieldEntryColumn(x => stringOptionToString(x.dataTypeSourceField))
-      case object FormatSourceField extends FieldEntryColumn(x => stringOptionToString(x.formatSourceField))
-      case object Tags extends FieldEntryColumn(_.tags.map(_.mkString(tagsSeparator)).getOrElse(new String))
-      case object FieldPositionInTheObject extends FieldEntryColumn(_.fieldPositionInTheObject.flatten.map(_.toString).getOrElse(new String))
-      case object ExcludeInclude extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.excludeInclude))
-      case object TokenizationType extends FieldEntryColumn(x => stringOptionToString(x.tokenizationType))
-      case object RegistrationDate extends FieldEntryColumn(_.registrationDate.map(_.format(DateTimeFormatter.ofPattern(registrationDateFormat))).getOrElse(new String))
-      case object CountryTheConceptualEntity extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.countryTheConceptualEntity))
-      case object ConceptualEntity extends FieldEntryColumn(x => stringOptionToString(x.conceptualEntity))
-      case object OperationalEntity extends FieldEntryColumn(x => stringOptionToString(x.operationalEntity))
-      case object Tds extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.tds))
+    sealed class FieldEntryColumn(val string: FieldEntry => String) extends AbstractColumn[FieldEntry]
 
 
-      val order = Seq(Country, PhysicalNameObject, StorageType, StorageZone, PhysicalNameField, LogicalNameField, SimpleFieldDescription, Catalog, DataType, Format, LogicalFormat, Key, Mandatory, DefaultValue, PhysicalNameSourceObject, SourceField,
-        DataTypeSourceField, FormatSourceField, Tags, FieldPositionInTheObject, ExcludeInclude, TokenizationType, RegistrationDate, CountryTheConceptualEntity, ConceptualEntity, OperationalEntity, Tds
-      )
+    case object Country extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.country))
+    case object PhysicalNameObject extends FieldEntryColumn(x => stringOptionToString(x.physicalNameObject))
+    case object StorageType extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.storageType))
+    case object StorageZone extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.storageZone))
+    case object PhysicalNameField extends FieldEntryColumn(x => stringOptionToString(x.physicalNameField))
+    case object LogicalNameField extends FieldEntryColumn(x => stringOptionToString(x.logicalNameField))
+    case object SimpleFieldDescription extends FieldEntryColumn(x => stringOptionToString(x.simpleFieldDescription))
+    case object Catalog extends FieldEntryColumn(x => stringOptionToString(x.catalog))
+    case object DataType extends FieldEntryColumn(x => stringOptionToString(x.dataType))
+    case object Format extends FieldEntryColumn(x => stringOptionToString(x.format))
+    case object LogicalFormat extends FieldEntryColumn(x => stringOptionToString(x.logicalFormat))
+    case object Key extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.key))
+    case object Mandatory extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.mandatory))
+    case object DefaultValue extends FieldEntryColumn(x => stringOptionToString(x.defaultValue))
+    case object PhysicalNameSourceObject extends FieldEntryColumn(x => stringOptionToString(x.physicalNameSourceObject))
+    case object SourceField extends FieldEntryColumn(x => stringOptionToString(x.sourceField))
+    case object DataTypeSourceField extends FieldEntryColumn(x => stringOptionToString(x.dataTypeSourceField))
+    case object FormatSourceField extends FieldEntryColumn(x => stringOptionToString(x.formatSourceField))
+    case object Tags extends FieldEntryColumn(_.tags.map(_.mkString(tagsSeparator)).getOrElse(new String))
+    case object FieldPositionInTheObject extends FieldEntryColumn(_.fieldPositionInTheObject.flatten.map(_.toString).getOrElse(new String))
+    case object ExcludeInclude extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.excludeInclude))
+    case object TokenizationType extends FieldEntryColumn(x => stringOptionToString(x.tokenizationType))
+    case object RegistrationDate extends FieldEntryColumn(_.registrationDate.map(_.format(DateTimeFormatter.ofPattern(registrationDateFormat))).getOrElse(new String))
+    case object CountryTheConceptualEntity extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.countryTheConceptualEntity))
+    case object ConceptualEntity extends FieldEntryColumn(x => stringOptionToString(x.conceptualEntity))
+    case object OperationalEntity extends FieldEntryColumn(x => stringOptionToString(x.operationalEntity))
+    case object Tds extends FieldEntryColumn(x => enumeratedTypeOptionToString(x.tds))
 
 
-      private def stringOptionToString(x: Option[String]): String = {
-        x.getOrElse(new String)
-      }
+    val order = Seq(Country, PhysicalNameObject, StorageType, StorageZone, PhysicalNameField, LogicalNameField, SimpleFieldDescription, Catalog, DataType, Format, LogicalFormat, Key, Mandatory, DefaultValue, PhysicalNameSourceObject, SourceField,
+      DataTypeSourceField, FormatSourceField, Tags, FieldPositionInTheObject, ExcludeInclude, TokenizationType, RegistrationDate, CountryTheConceptualEntity, ConceptualEntity, OperationalEntity, Tds
+    )
 
+
+    private def stringOptionToString(x: Option[String]): String = {
+      x.getOrElse(new String)
     }
 
+  }
 
-    private val tagsSeparator = ";"
-    private val registrationDateFormat = "dd-MM-yyyy"
+
+  private val tagsSeparator = ";"
+  private val registrationDateFormat = "dd-MM-yyyy"
+  //todo possibly generalize this as it may probably be needed in many other places
+  private val googleSpreadsheetModifiedDateFormat = "d/M/yyyy"
 
 }
