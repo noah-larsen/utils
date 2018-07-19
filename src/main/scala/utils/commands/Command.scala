3d9848b786b21f5697a72ef8d7be4954559adb17
Noah Larsen <noah.larsen@bbva.com>
2018-07-19 14:52:13 -0500

    refactored Command

--- src/main/scala/utils/commands/Command.scala
+++ src/main/scala/utils/commands/Command.scala
@@ -1,30 +1,20 @@
 package utils.commands
 
+import utils.enumerated.EnumeratedType
+import utils.enumerated.EnumeratedType.NameFormats.{CaseFormats, ObjectNameWithSpacesBetweenWords}
+
 abstract class Command(
                         val letterName: Option[Char],
                         val parameters: Seq[Parameter] = Seq()
-                      ) {
-
-  def description: String = {
-    descriptionFromClassName
-  }
-
+                      ) extends EnumeratedType(ObjectNameWithSpacesBetweenWords(CaseFormats.Lowercase)) {
 
   def usage: String = {
     val descriptionParametersSeparator = ": "
     val parametersSeparator = " "
     val listParameterSuffix = " ..."
     val countingNumberRangedNamedCommandDescriptionSuffix = s" $countingNumberRangedNamedCommandUsageSymbol"
-    s"${letterName.getOrElse(countingNumberRangedNamedCommandUsageSymbol)} - $description${letterName.map(_ => new String).getOrElse(countingNumberRangedNamedCommandDescriptionSuffix)}${parameters.headOption.map(_ => descriptionParametersSeparator + parameters
+    s"${letterName.getOrElse(countingNumberRangedNamedCommandUsageSymbol)} - $name${letterName.map(_ => new String).getOrElse(countingNumberRangedNamedCommandDescriptionSuffix)}${parameters.headOption.map(_ => descriptionParametersSeparator + parameters
       .map(x => x.name + (if(x.isList) listParameterSuffix else new String)).mkString(parametersSeparator)).getOrElse(new String)}"
-  }
-
-
-  private def descriptionFromClassName: String = {
-    val wordSeparator = " "
-    val multipleClassNameAddedDisambiguationSymbol = "$"
-    val classNameSeparatorRE = "[.$]"
-    getClass.getName.split(classNameSeparatorRE).filter(_.nonEmpty).last.zipWithIndex.flatMap(y => Some(Unit).filter(_ => y._1.isUpper && y._2 != 0).map(_ => wordSeparator).getOrElse(new String) + y._1.toLower).mkString
   }
 
 
--- src/main/scala/utils/enumerated/Enumerated.scala
+++ src/main/scala/utils/enumerated/Enumerated.scala
@@ -6,11 +6,6 @@ trait Enumerated {
   val values: Seq[T]
 
 
-  def contains(name: String): Boolean = {
-    withName(name).isDefined
-  }
-
-
   def withName(name: String, ignoreCase: Boolean = true): Option[T] = {
     values.find(x => if(ignoreCase) x.name.equalsIgnoreCase(name) else x.name == name)
   }
