abbe730bb2a4c2c21cbc9e0dcc90799e6d25f9f8
Noah Larsen <noah_larsen@outlook.com>
2018-10-13 01:25:44 -0500

    towards timer

--- other/Timer.scala
+++ other/Timer.scala
@@ -35,8 +35,14 @@ case class Timer(private val prefix: String = new String) {
       }
     }
 
+
     val chronoUnits = Seq(ChronoUnit.DAYS, ChronoUnit.HOURS, ChronoUnit.MINUTES, ChronoUnit.SECONDS, ChronoUnit.MILLIS)
-    Display.withSpaces(convert(nanos, chronoUnits, maxNRelevantUnits).map(x => x._1.toString + display(x._2)))
+    def toSeconds(value: Long, units: ChronoUnit): Double = units.getDuration.multipliedBy(value).toNanos.toDouble / TimeUnit.SECONDS.toNanos(1)
+    convert(nanos, chronoUnits, maxNRelevantUnits) match {
+      case x if x.headOption.exists(_._2 == ChronoUnit.SECONDS) && x.length > 1 => toSeconds(x.head._1, x.head._2) + toSeconds(x.tail.head._1, x.tail.head._2) + display(
+        ChronoUnit.SECONDS)
+      case x => Display.withSpaces(x.map(y => y._1.toString + display(y._2)))
+    }
 
   }
 
@@ -56,7 +62,7 @@ case class Timer(private val prefix: String = new String) {
 
 
     val duration = Duration.ofNanos(nanos)
-    val relevantChronoUnits = Some(chronoUnits.filter(convert(duration, _) > 0).take(maxNRelevantUnits)).getOrElse(Seq(ChronoUnit.NANOS))
+    val relevantChronoUnits = Some(chronoUnits.sorted.reverse.filter(convert(duration, _) > 0).take(maxNRelevantUnits)).getOrElse(Seq(ChronoUnit.NANOS))
     relevantChronoUnits.tail.scanLeft((nanos, convert(duration, relevantChronoUnits.head), relevantChronoUnits.head))((x, y) => nanos - Duration.of(x._2, x._3).toNanos match {
       case z => (z, convert(Duration.ofNanos(z), y), y)}).map(x => (x._2, x._3))
 
