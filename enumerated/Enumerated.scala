fb3926dba3e7c11856dbabdfa0932f494f228502
Noah Larsen <noah.larsen@bbva.com>
2018-11-06 17:23:08 -0600

    added documentation

--- enumerated/Enumerated.scala
+++ enumerated/Enumerated.scala
@@ -1,10 +1,16 @@
 package utils.enumerated
 
-
-import scala.reflect.ClassTag
 import scala.reflect.api.JavaUniverse
-import scala.util.{Success, Try}
+import scala.util.Try
 
+/**
+  * An enumeration trait that combines the more straightforward typing and access to sealed types of the typed-objects within objects
+  * approach with the access to self-referentially named strings and collections of the enumerated values of the Enumeration class. The
+  * enumerated values are instances of the type member, and must be objects declared directly inside an Enumerated object. The order of the
+  * object declarations determines the order within the output of the values method. The trait does require a small amount of boilerplate.
+  * After defining the type member, set enumeratedTypes equal to EnumeratedTypes(u.typeOf[ENUMERATED_OBJECT.type], classOf[TYPE_MEMBER])
+  * with the corresponding values for ENUMERATED_OBJECT and TYPE_MEMBER.
+  */
 trait Enumerated {
 
   type T <: SelfNamed
@@ -18,9 +24,16 @@ trait Enumerated {
 
 
   def withName(name: String, ignoreCase: Boolean = true, normalizeSpaceChars: Boolean = true, trim: Boolean = true): Option[T] = {
-    val normalSpace = " "
-    def process(value: String) = Some(value).map(x => if(ignoreCase) x.toLowerCase else x).map(x => if(normalizeSpaceChars) x.map(y => if(y.isSpaceChar) normalSpace else y).mkString else x).map(x => if(trim) x.trim else x).get
+
+    def process(value: String) = {
+      val normalSpace = " "
+      Some(value).map(x => if(ignoreCase) x.toLowerCase else x).map(x => if(normalizeSpaceChars) x.map(y => if(y.isSpaceChar)
+        normalSpace else y).mkString else x).map(x => if(trim) x.trim else x).get
+    }
+
+
     values.find(x => process(x.name) == process(name))
+
   }
 
 
